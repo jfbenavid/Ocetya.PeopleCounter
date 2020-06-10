4de5c704b908d92d8c7fc1061aeaaa0a3944c07d
Fabian Benavides <jfbenavid@hotmail.com>
2020-06-10 09:04:53 -0500

    Adding all the docs to git.

---
+++ GranEstacion.Web/ClientApp/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Container } from 'reactstrap';
+import { Home } from './components/Home';
+
+import './custom.css'
+
+const App = () => {
+  return (
+    <Container>
+      <Home />
+    </Container>
+  );
+}
+
+export default App;
---
+++ GranEstacion.Web/ClientApp/src/index.js
@@ -0,0 +1,13 @@
+import 'bootstrap/dist/css/bootstrap.css';
+import React from 'react';
+import { render } from 'react-dom';
+import App from './App';
+import registerServiceWorker from './registerServiceWorker';
+
+const rootElement = document.getElementById('root');
+
+render(
+  <App />,
+  rootElement);
+
+registerServiceWorker();
